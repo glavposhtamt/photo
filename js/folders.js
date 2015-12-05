9bd6d84c3500b75d6e644c9dd33b518920101c25
Sergey <glavposhtamt@bk.ru>
2015-12-05 22:51:41 +0300

    render to object

--- js/folders.js
+++ js/folders.js
@@ -236,10 +236,10 @@ folders.scanDir = function(){
 
 					if (rendered.length) {
 						currentPath = hash[0];
-						render(rendered);
+						that.render(rendered);
 					}
 					else {
-						render(rendered);
+						that.render(rendered);
 					}
 
 				}
@@ -254,13 +254,13 @@ folders.scanDir = function(){
 
 						currentPath = hash[0];
 						that.breadcrumbsUrls = that.generateBreadcrumbs(hash[0]);
-						render(rendered);
+						that.render(rendered);
 
 					}
 					else {
 						currentPath = hash[0];
 						that.breadcrumbsUrls = that.generateBreadcrumbs(hash[0]);
-						render(rendered);
+						that.render(rendered);
 					}
 
 				}
@@ -270,91 +270,87 @@ folders.scanDir = function(){
 				else {
 					currentPath = data.path;
 					that.breadcrumbsUrls.push(data.path);
-					render(that.searchByPath(data.path, response));
+					that.render(that.searchByPath(data.path, response));
 				}
 			}
 		}
+        
+	});
+};
 
+// Render the HTML for the file manager
 
-		// Render the HTML for the file manager
+folders.render = function(data) {
 
-		function render(data) {
+    var scannedFolders = [],
+	    scannedFiles = [],
+        that = this;
 
-			var scannedFolders = [],
-				scannedFiles = [];
+    if(Array.isArray(data)) {
 
-			if(Array.isArray(data)) {
+	   data.forEach(function (d) {
+           if (d.type === 'folder') {
+               scannedFolders.push(d);
+           } else if (d.type === 'file') {
+               scannedFiles.push(d);
+           }
 
-				data.forEach(function (d) {
+       });
 
-					if (d.type === 'folder') {
-						scannedFolders.push(d);
-					}
-					else if (d.type === 'file') {
-						scannedFiles.push(d);
-					}
+    } else if(typeof data === 'object') {
 
-				});
+        scannedFolders = data.folders;
+		scannedFiles = data.files;
+    }
 
-			}
-			else if(typeof data === 'object') {
+// Empty the old result and make the new one
 
-				scannedFolders = data.folders;
-				scannedFiles = data.files;
+	that.fileList.empty().hide();
 
-			}
+	if(!scannedFolders.length && !scannedFiles.length) {
+        that.filemanager.find('.nothingfound').show();
+    } else {
+	   that.filemanager.find('.nothingfound').hide();
+    }
 
-            // Empty the old result and make the new one
+	if(scannedFolders.length) {
 
-			that.fileList.empty().hide();
+	   scannedFolders.forEach(function(f) {
 
-			if(!scannedFolders.length && !scannedFiles.length) {
-				that.filemanager.find('.nothingfound').show();
-			}
-			else {
-				that.filemanager.find('.nothingfound').hide();
-			}
+		  var itemsLength = f.items.length,
+              name = that.escapeHTML(f.name),
+              icon = '<span class="icon folder"></span>';
 
-			if(scannedFolders.length) {
+           if(itemsLength) {
+               icon = '<span class="icon folder full"></span>';
+           }
 
-				scannedFolders.forEach(function(f) {
+           if(itemsLength == 1) {
+               itemsLength += ' item';
+           } else if(itemsLength > 1) {
+               itemsLength += ' items';
+           } else {
+               itemsLength = 'Empty';
+           }
 
-					var itemsLength = f.items.length,
-						name = that.escapeHTML(f.name),
-						icon = '<span class="icon folder"></span>';
-
-					if(itemsLength) {
-						icon = '<span class="icon folder full"></span>';
-					}
-
-					if(itemsLength == 1) {
-						itemsLength += ' item';
-					}
-					else if(itemsLength > 1) {
-						itemsLength += ' items';
-					}
-					else {
-						itemsLength = 'Empty';
-					}
-
-					var folder = $('<li class="folders"><a href="'+ f.path +'" title="'+ f.path +'" class="folders">'+icon+'<span class="name">' + name + '</span> <span class="details">' + itemsLength + '</span></a></li>');
+           var folder = $('<li class="folders"><a href="'+ f.path +'" title="'+ f.path +'" class="folders">'+icon+'<span class="name">' + name + '</span> <span class="details">' + itemsLength + '</span></a></li>');
 					folder.appendTo(that.fileList);
-				});
+       });
 
-			}
+    }
 
-			if(scannedFiles.length) {
+	if(scannedFiles.length) {
 
-				scannedFiles.forEach(function(f) {
+	   scannedFiles.forEach(function(f) {
 
-					var fileSize = that.bytesToSize(f.size),
-						name = that.escapeHTML(f.name),
-						fileType = name.split('.'),
-						icon = '<span class="icon file"></span>',
-                        id = f.id,
-                        url = 'http://' + location.hostname + '/' + decodeURI(f.url);
+           var fileSize = that.bytesToSize(f.size),
+               name = that.escapeHTML(f.name),
+               fileType = name.split('.'),
+               icon = '<span class="icon file"></span>',
+               id = f.id,
+               url = 'http://' + location.hostname + '/' + decodeURI(f.url);
 
-					fileType = fileType[fileType.length-1];
+               fileType = fileType[fileType.length-1];
 
                     var href = 'http://' + location.hostname +'/files/.thumbail/' + name;
                     icon = '<img class="icon thumbnail" src="'+ decodeURI(href) + '" >';
@@ -368,50 +364,46 @@ folders.scanDir = function(){
 					file.appendTo(that.fileList);
 				});
 
-			}
+    }
 
 
-			// Generate the breadcrumbs
+	// Generate the breadcrumbs
 
-			var url = '';
+    var url = '';
 
-			if(that.filemanager.hasClass('searching')){
-
-				url = '<span>Search results: </span>';
-				that.fileList.removeClass('animated');
-
-			}
-			else {
-
-				that.fileList.addClass('animated');
-
-				that.breadcrumbsUrls.forEach(function (u, i) {
-
-					var name = u.split('/');
-
-					if (i !== that.breadcrumbsUrls.length - 1) {
-						url += '<a href="'+u+'"><span class="folderName">' + name[name.length-1] + '</span></a> <span class="arrow">→</span> ';
-					}
-					else {
-						url += '<span class="folderName">' + name[name.length-1] + '</span>';
-					}
-
-				});
-
-			}
-
-			that.breadcrumbs.text('').append(url);
-
-
-			// Show the generated elements
-
-			that.fileList.animate({'display':'inline-block'});
-            //that.fileList.show();
-            
-            that.replaceFile();
-            that.setUploadPath();
-
-		}
+	if(that.filemanager.hasClass('searching')){
         
-	});
+        url = '<span>Search results: </span>';
+		that.fileList.removeClass('animated');
+
+    } else {
+
+	   that.fileList.addClass('animated');
+       that.breadcrumbsUrls.forEach(function (u, i) {
+
+           var name = u.split('/');
+
+           if (i !== that.breadcrumbsUrls.length - 1) {
+               url += '<a href="'+u+'"><span class="folderName">' + name[name.length-1] + 
+                      '</span></a> <span class="arrow">→</span> ';
+               
+           } else {
+               url += '<span class="folderName">' + name[name.length-1] + '</span>';
+           }
+
+       });
+
+    }
+
+	that.breadcrumbs.text('').append(url);
+
+
+	// Show the generated elements
+
+	that.fileList.animate({'display':'inline-block'});
+     //that.fileList.show();
+            
+     that.replaceFile();
+     that.setUploadPath();
+
 };
