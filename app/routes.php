e9781adf2ccec7c5fbd529cd2aa9a80a04ad5b43
Sergey <glavposhtamt@bk.ru>
2015-12-22 23:31:48 +0300

    function auth

--- app/routes.php
+++ app/routes.php
@@ -2,6 +2,16 @@
 
 $js_css = $app->config('js_css');
 
+$authenticate = function ($app) {
+    return function () use ($app) {
+        if (!isset($_SESSION['user'])) {
+            $_SESSION['urlRedirect'] = $app->request()->getPathInfo();
+            $app->flash('error', 'Login required');
+            $app->redirect('/login');
+        }
+    };
+};
+
 require_once 'Routes/routSite.php';
 require_once 'Routes/routAdmin.php';
 require_once 'Routes/routFile.php';
