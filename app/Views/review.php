38d835573deaeb2b33234acc689535c61eb62924
Sergey <glavposhtamt@bk.ru>
2015-12-25 19:30:31 +0300

    add review

---
+++ app/Views/review.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="http://startupfoto.ru/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
+    <meta charset="UTF-8">
+
+    <?php  addCssAndJs($jsCSSLibs, ['default-template']);  ?>
+
+        <title>
+            <?php if(!isset($title)) $title = 'Главная страница - StartUP'; echo $title;?>
+        </title>
+
+</head>
+
+<body>
+    <div class="wrap">
+        <?php require 'header.php' ?>
+
+            <div id="b1">
+
+                <?php require 'topmenu.php'; ?>
+
+                <?php require 'menu.php'; ?>
+
+               <div class="item-pagebook1">
+                   <div class="text page">
+                       <div class="comment_sub_menu">
+                           <table class="menu_items">
+                               <tbody>
+                                   <tr>
+                                       <td class="menu_item">
+                                           <a href="/review" class="link <?=$type == 'r' ? 'active' : '' ?>">Оставить отзыв</a>
+                                       </td>
+                                       <td class="menu_item">
+                                           <a href="/question" class="link <?=$type == 'q' ? 'active' : '' ?>">Спросить совет</a>
+                                       </td>
+                                   </tr>
+                               </tbody>
+                           </table>
+                       </div>
+                       <div class="comments">
+                           <div class="red_popup review_add">
+                               <form id="TComments5" action="/actions.php" class="form" method="post">
+                                   <input name="mode" value="TComments" type="hidden">
+                                   <input name="parent" value="5" type="hidden">
+                                   <input name="do" value="add" type="hidden">
+                                   <input name="answer_to" value="0" type="hidden">
+                                   <input name="type" value="review" type="hidden">
+                                   <div class="field">
+                                       <div class="name">ФИО: <span class="star">*</span>: </div>
+                                       <div class="val">
+                                           <input name="author" placeholder="ФИО" required="" type="text">
+                                       </div>
+                                   </div>
+                                   <div class="field">
+                                       <div class="name">E-mail <span class="star">*</span>: </div>
+                                       <div class="val">
+                                           <input name="email" placeholder="e-mail@domain.com" required="" type="text">
+                                       </div>
+                                   </div>
+                                   <div class="field">
+                                       <div class="name">Телефон: </div>
+                                       <div class="val">
+                                           <input name="phone" placeholder="" type="text">
+                                       </div>
+                                   </div>
+                                   <div class="field">
+                                       <div class="name">Учебное заведение: </div>
+                                       <div class="val">
+                                           <input name="institution" placeholder="" type="text">
+                                       </div>
+                                   </div>
+                                   <div class="field">
+                                       <div class="name">Класс: </div>
+                                       <div class="val">
+                                           <input name="class" placeholder="" type="text">
+                                       </div>
+                                   </div>
+                                   <div class="field">
+                                       <div class="name"><?=$type == 'r' ? 'Отзыв' : 'Вопрос'; ?>: </div>
+                                       <div class="val">
+                                           <textarea name="text" required=""></textarea>
+                                       </div>
+                                   </div>
+                                   <div class="submit" style="text-align:center;">
+                                       <input class="send" value="Добавить" type="submit">
+                                   </div>
+                               </form>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+               
+               <?php require 'footer.php' ?>
+
+            </div>
+        </div>
+</body>
